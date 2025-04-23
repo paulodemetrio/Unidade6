2ce2687902bd6b0a81d99eb6d888f0b04ec10d1f
paulodemetrio <pdemetriocom@gmail.com>
2025-04-23 19:18:24 -0300

    teste

---
+++ src/Funcao.java
@@ -0,0 +1,10 @@
+public class Funcao {
+    public static void main(String[] args) {
+        double valor = exemplo.somarDecimal(7.3, 4.9);
+        System.out.println("Soma é " + valor);
+    }
+
+    private double somarDecimal(double v1, double v2) {
+        return v1 + v2;
+    }
+}
