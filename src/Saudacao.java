08cee0935659825d8f43b2cae5099b59b2c28cc3
paulodemetrio <pdemetriocom@gmail.com>
2025-05-06 20:31:21 -0300

    teste

--- src/Saudacao.java
+++
@@ -1,9 +0,0 @@
-// public class Saudacao {
-//     public static void main(String[] args) {
-//         saudacao();
-//     }
-
-//     public static void saudacao() {
-//         System.out.println("Olá, mundo!");
-//     }
-// }
